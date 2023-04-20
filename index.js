475987178cc9aae6ee02e40cd611f4c8e6b46fcd
Jonas Lima de Amorim <dev.amorim@outlook.com>
2023-04-20 13:24:53 -0300

    feat: including stage and processed folders

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const fs = require('fs');
+const path = require('path');
 const axios = require('axios');
-require('dotenv').config();
-
 const winston = require('winston');
+
+const FILE_EXTENSION = '.json';
+const STAGE_PATH = './models/stage/';
+const PROCESSED_PATH = './models/processed/';
+
+if (!fs.existsSync(PROCESSED_PATH)){
+  fs.mkdirSync(PROCESSED_PATH, { recursive: true });
+}
+
+require('dotenv').config();
 
 const reqlogger = winston.createLogger({
   level: 'info',
@@ -23,38 +32,57 @@ const reslogger = winston.createLogger({
   });
 
 let activities = JSON.parse(fs.readFileSync('data/activities.json'));
-let data = JSON.parse(fs.readFileSync('models/activities.json'));
 
-for (const activity of activities.values()) {
+fs.readdir(STAGE_PATH, (err, files) => {
+    if (!files) throw new Error("No files available");
+    
+    const targetFiles = files.filter(file => {
+        return path.extname(file).toLowerCase() === FILE_EXTENSION;
+    });
+    
+    for (const file of targetFiles.values()) {
+      let data = JSON.parse(fs.readFileSync(`${STAGE_PATH}${file}`));
 
-    let worklogs = data[activity.name]
-
-    if (worklogs && worklogs.length > 0) {
-
-        for (const worklog of worklogs.values()) {
-
-            let req = {
-                timeStamp: worklog.date,
-                length: worklog.length * 60 * 60,
-                billableLength: null,
-                workItemId: worklog.workItemId,
-                comment: worklog.comment,
-                userId: process.env.TIMETRACKER_USER_ID,
-                activityTypeId: activity.id,
-            }
-
-            reqlogger.info(JSON.stringify(req))
-            
-            const config = {
-                headers:{
-                  Authorization: `Bearer ${process.env.TIMETRACKER_BEARER_TOKEN}`
-                }
-              };
-
-            axios.post(process.env.TIMETRACKER_URL, req, config).then((res) => {
-                reslogger.info(`HTTP ${res.status} | ${JSON.stringify(res.data)}`)
-            });
-
+      for (const activity of activities.values()) {
+      
+          let worklogs = data[activity.name]
+      
+          if (worklogs && worklogs.length > 0) {
+      
+              for (const worklog of worklogs.values()) {
+      
+                  let req = {
+                      timeStamp: worklog.date,
+                      length: worklog.length * 60 * 60,
+                      billableLength: null,
+                      workItemId: worklog.workItemId,
+                      comment: worklog.comment,
+                      userId: process.env.TIMETRACKER_USER_ID,
+                      activityTypeId: activity.id,
+                  }
+      
+                  reqlogger.info(JSON.stringify(req))
+                  
+                  const config = {
+                      headers:{
+                        Authorization: `Bearer ${process.env.TIMETRACKER_BEARER_TOKEN}`
+                      }
+                    };
+      
+                  axios.post(process.env.TIMETRACKER_URL, req, config).then((res) => {
+                      reslogger.info(`HTTP ${res.status} | ${JSON.stringify(res.data)}`)
+                  });
+      
+              }
         }
+      }
+      
+      const oldPath = `${STAGE_PATH}${file}`;
+      const newPath = `${PROCESSED_PATH}${file}`;
+      
+      fs.rename(oldPath, newPath, function (err) {
+          if (err) throw err
+          console.log('Successfully renamed - AKA moved!')
+        });
     }
-}
+});
